fb256f8a2ea6b09596154d30c601dd7f0cbebe24
umaka <umaka@DESKTOP-G3O80KM>
2017-02-20 12:19:26 -0600

    Add project files.

---
+++ exception/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader str = null;
+            try
+            {
+
+                 str = new StreamReader(@"C:\softwares\SQL software\excep1.txt");
+                Console.WriteLine(str.ReadToEnd());
+                str.Close();
+                
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine("Please enter the correct filename {0}", ex.Message);
+                Console.ReadLine();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+                {
+                if(str!=null)
+                {
+                    str.Close();
+
+                }
+            }
+        }
+        
+    }
+}
